db12266c33bfc6e9054555d82bd358be5a712f3c
krakjoe <joe.watkins@live.co.uk>
2013-01-27 04:38:19 +0000

    operators test file

--- operators.php
+++ operators.php
@@ -16,9 +16,46 @@ class operable {
 			case OPERATORS_SL: return $this->value << $zval; break;
 			case OPERATORS_SR: return $this->value >> $zval; break;
 			case OPERATORS_CONCAT: return $this->value . $zval; break;
-			case OPERATORS_BW_OR: return $this->value || $zval; break;
+			case OPERATORS_BW_OR: return $this->value | $zval; break;
 			case OPERATORS_BW_AND: return $this->value & $zval; break;
-			case OPERATORS_BW_XOR: return $this->value | $zval; break;
+			case OPERATORS_BW_XOR: return $this->value ^ $zval; break;
+
+			case OPERATOR_IDENTICAL: return $this->value === $zval; break;
+			case OPERATOR_NOT_IDENTICAL: return $this->value !== $zval; break;
+			case OPERATOR_IS_EQUAL: return $this->value == $zval; break;
+			case OPERATOR_IS_NOT_EQUAL: return $this->value != $zval; break;
+	
+			/** assign should return zero at the moment */
+			case OPERATOR_ASSIGN_ADD:
+				$this->value += $zval;
+				return 0;
+			case OPERATOR_ASSIGN_SUB:
+				$this->value -= $zval;
+				return 0;
+			case OPERATOR_ASSIGN_DIV:
+				$this->value /= $zval;
+				return 0;
+			case OPERATOR_ASSIGN_MOD:
+				$this->value %= $zval;
+				return 0;
+			case OPERATOR_ASSIGN_SL:
+				$this->value <<= $zval;
+				return 0;
+			case OPERATOR_ASSIGN_SR:
+				$this->value >>= $zval;
+				return 0;
+			case OPERATOR_ASSIGN_CONCAT:
+				$this->value .= $zval;
+				return 0;
+			case OPERATOR_ASSIGN_BW_OR:
+				$this->value |= $zval;
+				return 0;
+			case OPERATOR_ASSIGN_BW_AND:
+				$this->value &= $zval;
+				return 0;
+			case OPERATOR_ASSIGN_BW_XOR:
+				$this->value ^= $zval;
+				return 0;
 
 			default: 
 				throw new Exception("Unsupported operator");
@@ -36,6 +73,14 @@ $c = $m / $a;
 
 $d = $b * $m;
 
+
+$d += $a;
+
+$d |= $a;
+
+$d <<= $b;
+
+$d++;
 
 var_dump($b, $c, $d, $m);
 ?>
