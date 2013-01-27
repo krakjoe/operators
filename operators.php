e562e7e79ab88a48c1fae399eebe4ac3c8ede5c7
krakjoe <joe.watkins@live.co.uk>
2013-01-27 04:14:49 +0000

    rest

--- operators.php
+++ operators.php
@@ -8,8 +8,18 @@ class operable {
 	
 	public function __operators($opcode, $zval = null) {
 		switch($opcode) {
+
 			case OPERATORS_ADD: return $this->value + $zval; break;
-			
+			case OPERATORS_SUB: return $this->value - $zval; break;
+			case OPERATORS_MUL: return $this->value * $zval; break;
+			case OPERATORS_DIV: return $this->value / $zval; break;
+			case OPERATORS_SL: return $this->value << $zval; break;
+			case OPERATORS_SR: return $this->value >> $zval; break;
+			case OPERATORS_CONCAT: return $this->value . $zval; break;
+			case OPERATORS_BW_OR: return $this->value || $zval; break;
+			case OPERATORS_BW_AND: return $this->value & $zval; break;
+			case OPERATORS_BW_XOR: return $this->value | $zval; break;
+
 			default: 
 				throw new Exception("Unsupported operator");
 		}
@@ -21,7 +31,11 @@ $m = new operable(5);
 $a = 5;
 
 $b = $m + $a;
-$c = $m + $a;
 
-var_dump($b, $c, $m);
+$c = $m / $a;
+
+$d = $b * $m;
+
+
+var_dump($b, $c, $d, $m);
 ?>
